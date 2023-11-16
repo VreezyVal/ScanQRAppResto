e5df8f625f57f6ab96868615aaac1877e0243140
VreezyVal <valmydituluakidi@gmail.com>
2023-11-17 00:59:08 +0100

    first commit

---
+++ client/src/serviceWorker.js
@@ -0,0 +1,105 @@
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+      // [::1] est l'adresse IPv6 de localhost.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.0/8 est considéré comme un réseau local.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+  );
+  
+  export function register(config) {
+    if ('serviceWorker' in navigator) {
+      const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+      if (publicUrl.origin !== window.location.origin) {
+        return;
+      }
+  
+      window.addEventListener('load', () => {
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+  
+        if (isLocalhost) {
+          checkValidServiceWorker(swUrl, config);
+          navigator.serviceWorker.ready.then(() => {
+            console.log(
+              'Ce site web est en mode de développement. Service worker non enregistré.'
+            );
+          });
+        } else {
+          registerValidSW(swUrl, config);
+        }
+      });
+    }
+  }
+  
+  function registerValidSW(swUrl, config) {
+    navigator.serviceWorker
+      .register(swUrl)
+      .then(registration => {
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          if (installingWorker == null) {
+            return;
+          }
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === 'installed') {
+              if (navigator.serviceWorker.controller) {
+                console.log(
+                  'Un nouveau contenu est disponible et sera utilisé lors de la prochaine navigation.'
+                );
+  
+                if (config && config.onUpdate) {
+                  config.onUpdate(registration);
+                }
+              } else {
+                console.log('Le contenu est en cache pour une utilisation hors ligne.');
+                if (config && config.onSuccess) {
+                  config.onSuccess(registration);
+                }
+              }
+            }
+          };
+        };
+      })
+      .catch(error => {
+        console.error('Erreur lors de l’enregistrement du service worker:', error);
+      });
+  }
+  
+  function checkValidServiceWorker(swUrl, config) {
+    fetch(swUrl, {
+      headers: { 'Service-Worker': 'script' },
+    })
+      .then(response => {
+        const contentType = response.headers.get('content-type');
+        if (
+          response.status === 404 ||
+          (contentType != null && contentType.indexOf('javascript') === -1)
+        ) {
+          navigator.serviceWorker.ready.then(registration => {
+            registration.unregister().then(() => {
+              window.location.reload();
+            });
+          });
+        } else {
+          registerValidSW(swUrl, config);
+        }
+      })
+      .catch(() => {
+        console.log(
+          'Aucune connexion Internet trouvée. Application chargée en mode hors ligne.'
+        );
+      });
+  }
+  
+  export function unregister() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready
+        .then(registration => {
+          registration.unregister();
+        })
+        .catch(error => {
+          console.error('Erreur lors de la désinscription du service worker:', error);
+        });
+    }
+  }
